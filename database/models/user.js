e7cbcf2de817182c79cc5684f7c8219f961e7962
wangpan <wpmale@gmail.com>
2019-04-10 14:19:55 +0800

    allow unionid null

--- database/models/user.js
+++ database/models/user.js
@@ -1,8 +1,11 @@
-let { defineModel } = require('../db');
+let {defineModel} = require('../db');
 const Sequelize = require('sequelize');
 module.exports = defineModel('user', {
     openid: Sequelize.DataTypes.STRING,
-    unionid: Sequelize.DataTypes.STRING,
+    unionid: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true
+    },
     wechatName: Sequelize.DataTypes.STRING,
     avatar: Sequelize.DataTypes.STRING
 });
